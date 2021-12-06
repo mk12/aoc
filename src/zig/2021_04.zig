8242011e3ffaec3773de1536d6167180db08bff3
Mitchell Kember <mk12360@gmail.com>
2021-12-06 11:42:15 -0800

    Start 2021_04.zig

---
+++ src/zig/2021_04.zig
@@ -0,0 +1,11 @@
+// Copyright 2021 Mitchell Kember. Subject to the MIT License.
+// Advent of Code 2021
+// Day 4: Giant Squid
+
+const std = @import("std");
+const fmt = std.fmt;
+const math = std.math;
+
+pub fn run(input: anytype, output: anytype) !void {
+    var buf: [128]u8 = undefined;
+}
